12b6bbcbebbe5fa607aaa43e60bb9cb27fcfe9bb
Johan Nordström <johan@digitalvillage.se>
2018-04-03 12:16:02 +0200

    Stöd för post thumbnails/featured images.

--- functions.php
+++ functions.php
@@ -14,3 +14,11 @@ function mbt_styles() {
 	wp_enqueue_style('mbt-styles', get_stylesheet_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'mbt_styles');
+
+function mbt_setup() {
+	add_theme_support('post-thumbnails');
+
+	// add some image sizes specific to our theme
+	add_image_size('post-featured-image', 2560, 500, true);
+}
+add_action('after_setup_theme', 'mbt_setup');
--- single.php
+++ single.php
@@ -2,12 +2,22 @@
 get_header(); // header.php
 ?>
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-9">
-			<?php if (have_posts()) { ?>
-				<!-- nu vet vi att det finns en eller flera poster -->
-				<?php while(have_posts()) { the_post(); ?>
+<?php if (have_posts()) { ?>
+	<!-- nu vet vi att det finns en eller flera poster -->
+	<?php while(have_posts()) { the_post();
+			if (has_post_thumbnail()) {
+				?>
+					<div class="featured-image-wrapper">
+						<!-- show post featured image -->
+						<?php the_post_thumbnail('post-featured-image'); ?>
+					</div>
+				<?php
+			}
+		?>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-9">
 					<!-- nu är vi inne på en enskild post -->
 					<h3><?php the_title(); ?></h3>
 					<?php the_content(); ?>
@@ -15,17 +25,17 @@ get_header(); // header.php
 					<p>
 						Skriven <?php the_date(); ?> av <?php the_author_posts_link(); ?> i <?php the_category(', '); ?>.
 					</p>
-				<?php } ?>
-			<?php } else { ?>
-				<!-- nu har vi konstaterat att det INTE finns någon post att hämta på denna sida -->
-				Ledsen, det finns inget innehåll på den här sidan.
-			<?php } ?>
+				</div>
+				<div class="col-md-3">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
 		</div>
-		<div class="col-md-3">
-			<?php get_sidebar(); ?>
-		</div>
-	</div>
-</div>
+	<?php } ?>
+<?php } else { ?>
+	<!-- nu har vi konstaterat att det INTE finns någon post att hämta på denna sida -->
+	Ledsen, det finns inget innehåll på den här sidan.
+<?php } ?>
 
 <?php
 get_footer();
