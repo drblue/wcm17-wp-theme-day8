207af3f27017848125837e757d1aa1426ea7d9b5
Johan Nordström <johan@digitalvillage.se>
2018-04-03 12:18:21 +0200

    Förbättrad laddning av Bootstrap CSS och JavaScript-bibliotek.

--- functions.php
+++ functions.php
@@ -11,7 +11,13 @@ function mbt_register_menus() {
 add_action('init', 'mbt_register_menus');
 
 function mbt_styles() {
+	wp_enqueue_style('bootstrap4-styles', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
 	wp_enqueue_style('mbt-styles', get_stylesheet_directory_uri() . '/style.css');
+
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', [], '3.2.1', true);
+	wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', ['jquery'], '1.12.9', true);
+	wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', ['jquery', 'popper'], '4.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'mbt_styles');
 
