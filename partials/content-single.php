e02be026d6641dadefdae6f2dc4819d52a1658cf
Johan Nordström <johan@digitalvillage.se>
2018-04-11 12:33:10 +0200

    Single post navigation.

--- partials/content-single.php
+++ partials/content-single.php
@@ -16,9 +16,16 @@
 			<h3><?php the_title(); ?></h3>
 			<?php the_content(); ?>
 			<hr>
-			<p>
-				Skriven <?php the_date(); ?> av <?php the_author_posts_link(); ?> i <?php the_category(', '); ?>.
-			</p>
+			<div class="post-footer text-muted small">
+				<div class="post-meta">
+					Skriven <?php the_date(); ?> av <?php the_author_posts_link(); ?> i <?php the_category(', '); ?>.
+				</div>
+				<div class="post-navigation d-flex justify-content-between">
+					<div class="nav-next alignright"><?php previous_post_link('%link', '&laquo; Föregående inlägg'); ?></div>
+
+					<div class="nav-previous alignleft"><?php next_post_link('%link', 'Nästa inlägg &raquo;'); ?></div>
+				</div>
+			</div>
 			<?php
 				get_template_part('partials/content', 'related'); // partials/content-related.php
 			?>
